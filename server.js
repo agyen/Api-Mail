363c6db9838677dc1f78ed3d7f292062be037818
Dilyen <davidandohacquah@gmail.com>
2020-01-22 17:30:20 +0000

    name of app

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/TCMSUI'));
+app.use(express.static(__dirname + '/dist/Api-Mail'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/TCMSUI/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Api-Mail/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
